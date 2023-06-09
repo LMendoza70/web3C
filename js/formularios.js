183e5d1e954dc1482ad6b1b6c54637081953d7aa
LMendoza70 <luis.mendoza@uthh.edu.mx>
2023-06-09 13:35:59 -0600

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   js/formularios.js
    
     Changes not staged for commit:
    	modified:   formularios.html

--- js/formularios.js
+++ js/formularios.js
@@ -1,15 +1,22 @@
 var formulario=document.getElementById("frmlogin")
-var uss="admin", pss="0000"
+var uss="admin", pss="0000",contador=0
+
+
 formulario.addEventListener("submit",function (evento){
     evento.preventDefault()
     var usuario= document.getElementById("user").value
     var password=document.getElementById("pass").value
+    var btn=document.getElementById("boton")
+    var url="../respuestas.html"
+    var parametros="?usuario="+encodeURIComponent(usuario)+"&contasenia="+encodeURIComponent(password)
 
     if(uss==usuario && pss==password){
-        window.location.href="../respuestas.html"
-    }else{
-        alert("Credenciales incorrectas")
-    }
+        window.location.href=url+parametros
+    }else if(contador<3){
+        contador=contador+1
+        alert("Credenciales incorrectas intento :"+contador)         
+    }else
+        btn.disabled=true
 
     console.log("hola")
 })
